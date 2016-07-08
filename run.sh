ac84e33cbc57122efe8b07f18427fc777793e2f8
Vitalii <v.hrytsiienko@gmail.com>
2016-07-07 17:15:24 -0700

    initial commit

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+export NODE_ENV="${NODE_ENV:-development}"
+
+if [ $NODE_ENV != "production" ]; then
+  npm run start
+else
+ #here we can use nginx for example
+  npm run start
+fi
